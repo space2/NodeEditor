ead2b20b96d3f11b087aa139be65c10d209b4e74
Pal Szasz <pal.szasz@sonymobile.com>
2014-03-24 18:33:34 +0100

    Show value for bit slots

--- src/ui/NodeUI.cpp
+++ src/ui/NodeUI.cpp
@@ -11,6 +11,18 @@
 
 #include "NodeUI.h"
 
+static const int kSlotColorsDefault[] = {40, 46, 50, FL_DARK3};
+static const int kSlotColors0[] = {40, 7, 7, FL_DARK3};
+static const int kSlotColors1[] = {40, 0, 0, FL_DARK3};
+
+static const int * choose_slot_colors(const Slot * slot)
+{
+	if (slot->type() == Slot::Bit) {
+		return slot->as_bit() ? kSlotColors1 : kSlotColors0;
+	}
+	return kSlotColorsDefault;
+}
+
 NodeUI::NodeUI(Node * node)
 	: _node(node), _sel(0), _high(0)
 {
@@ -57,27 +69,29 @@ void NodeUI::draw(int dx, int dy)
 	fl_xyline(x + 2, y + 13, x + w - 2);
 	fl_font(FL_HELVETICA, 8);
 	for (int i = 0; i < input_count(); i++) {
+		const int * slot_colors = choose_slot_colors(_node->input(i));
 		int xx = x;
 		int yy = dy + _node->input_y(i);
-		fl_color(40);
+		fl_color(slot_colors[0]);
 		fl_pie(xx-4, yy-4, 9, 9, -90, 90);
-		fl_color(46);
+		fl_color(slot_colors[1]);
 		fl_pie(xx-3, yy-3, 7, 7, -90, 90);
-		fl_color(50);
+		fl_color(slot_colors[2]);
 		fl_line(xx+1, yy-3, xx+1, yy+3);
-		fl_color(FL_DARK3);
+		fl_color(slot_colors[3]);
 		fl_draw(input_name(i), xx + 5, yy + 1);
 	}
 	for (int i = 0; i < output_count(); i++) {
+		const int * slot_colors = choose_slot_colors(_node->output(i));
 		int xx = x + w - 1;
 		int yy = dy + _node->output_y(i);
-		fl_color(40);
+		fl_color(slot_colors[0]);
 		fl_pie(xx-4, yy-4, 9, 9, 90, 270);
-		fl_color(46);
+		fl_color(slot_colors[1]);
 		fl_pie(xx-3, yy-3, 7, 7, 90, 270);
-		fl_color(40);
+		fl_color(slot_colors[2]);
 		fl_line(xx, yy-3, xx, yy+3);
-		fl_color(FL_DARK3);
+		fl_color(slot_colors[3]);
 		int tw = 0, th = 0;
 		fl_measure(output_name(i), tw, th, 0);
 		fl_draw(output_name(i), xx - 5 - tw, yy + 1);
